de1ef42cf7b9c5070f9e4346709eeabd74b7ad8b
cleverom <clevereziogor@gmail.com>
2020-12-26 11:42:38 +0100

    add javascript funtionality

---
+++ script/index.js
@@ -0,0 +1,53 @@
+let min = 1;
+let max = 10;
+let winningNum = 2;
+let guessLeft = 3;
+
+const game = document.querySelector("#game");
+const minNum = document.querySelector(".min-num");
+const maxNum = document.querySelector(".max-num");
+const guessBtn = document.querySelector("#guess-value");
+const guessInput = document.querySelector("#guess-input");
+const message = document.querySelector(".message");
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  if (guess === NaN || guess < min || guess > max) {
+    setMessage(`please enter a number between ${min} and ${max}`, "red");
+  }
+
+  if (guess === winningNum) {
+    gameOver(true, `${winningNum} is correct YOU WIN!`);
+  } else {
+    guessLeft -= 1;
+
+    if (guessLeft === 0) {
+      gameOver(
+        false,
+        `GAME OVER, YOU LOST! The correct number was ${winningNum}`
+      );
+    } else {
+      guessInput.style.borderColor = "red";
+      guessInput.value = "";
+      setMessage(`${guess} is not correct!, ${guessLeft} guesses left`, "red");
+    }
+  }
+});
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  message.style.color = color;
+  setMessage(msg);
+}
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
